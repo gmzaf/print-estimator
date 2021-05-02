2c9a85f4595f8e944b989927ab7e68012f0fb775
gmzaf <gmzaf@users.noreply.github.com>
2021-05-02 19:24:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//Variable Declarations
-let radioChoice, inputHeight, inputWidth, substrate, inputArea, billArea, printCost, cpi, inchHeight, inchWidth, errorNote;
+// #region	Variable Declarations
+let radioChoice, inputHeight, inputWidth, substrate, inputArea, billArea, printCost, cpi, inchHeight, inchWidth, estimate, errorNote;
 const costGroup01 = ["Archival Matte Light (230 gsm)", "Archival Textured Light (285 gsm)"];
 const costGroup02 = ["Archival Matte (310 gsm)"];
 const costGroup03 = ["Archival Textured (310 gsm)", "Archival Photo Gloss Baryta (320 gsm)", "Archival Canvas (450 gsm)"];
@@ -7,23 +7,34 @@ const costGroup04 = ["Metalic (290 gsm)", "Canvas (380 gsm)"];
 const costGroup05 = ["Matte Coated (180 gsm)", "Matte Fiber (200 gsm)", "Luster (240 gsm)", "Glossy (240 gsm)"];
 const paperMax24 = ["Matte Fiber (200 gsm)"];
 const paperMax36 = ["Archival Matte Light (230 gsm)"];
+const radios = document.querySelectorAll('input[type=radio]');
+const list = document.querySelector("#list");
+// const createLi = document.createElement("LI");
+// const textNode = document.createTextNode(`${estimate}`);
+//#endregion	
 
-
-resetFields = () => {
-  // document.querySelector("#radioin").checked = true; // Keeping measure units from last selection 
+const resetFields = () => {
+  // document.querySelector("#radioin").checked = true; // To reset measure units to INCHES, add id="radioin" to radio input  
   document.querySelector("#inputHeight").value = "";
   document.querySelector("#inputWidth").value = "";
   document.querySelector("#substrate").value = "not-valid";
+  //document.querySelector("#errorNote").innerHTML = ""
 }
 
-getInputs = () => {
+// Live update placeholders with measure units.
+radios.forEach(radio => radio.addEventListener('change', () => {
+  document.querySelector(`#inputHeight`).placeholder = radio.value;
+  document.querySelector(`#inputWidth`).placeholder = radio.value;
+}));
+
+const getInputs = () => {
   radioChoice = document.querySelector('input[type="radio"]:checked').value;
   inputHeight = document.querySelector("#inputHeight").valueAsNumber;
   inputWidth = document.querySelector("#inputWidth").valueAsNumber;
   substrate = document.querySelector("#substrate").value;
 }
 
-setCPI = function() {     // in no paper is selected, CPI = 0
+const setCPI = function() {     // in no paper is selected, CPI = 0
 if (costGroup01.includes(substrate)) {
   if (billArea<=100) {cpi = 1.3} 
   else if (100<billArea && billArea<=150) {cpi = 1.15} 
@@ -80,76 +91,101 @@ if (costGroup01.includes(substrate)) {
   else if (1500<billArea && billArea<=2000) {cpi = 0.7} 
   else if (2000<billArea) {cpi = 0.75} 
 } else {cpi = 0}
-console.log(`setCPI: billArea:${billArea}, substrate:${substrate}, cpi:${cpi}`)
 }
 
 function currencyFormat(num) {
   return 'HK$ ' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') 
-}
-
-logInputValues = () => {
-  console.log(`INPUT: ${inputHeight}${radioChoice} by ${inputWidth}${radioChoice} on ${substrate}`)
-}
-
-displayOutput = () => {
-  checkInput();
-  if (errorNote === "no errors") {
-    document.querySelector("#output").innerHTML = `
-    ${inputHeight}x${inputWidth}${radioChoice}<br>${substrate}<br><strong>${currencyFormat(printCost)}</strong>
-    `
-} else {
-  document.querySelector("#output").innerHTML = `<p style="color:red">${errorNote}</p>`
-}
-}
-
-checkInput = () => { // checking if inputs are valid and generate error messages
-  if (radioChoice === "mm") {
-    inchHeight = Math.ceil(inputHeight / 25.4);
-    inchWidth = Math.ceil(inputWidth / 25.4);
-  } else {
-    inchHeight = inputHeight;
-    inchWidth = inputWidth; 
-  };
-  if (!billArea>0 || substrate=== "not-valid") {
-    errorNote = `Missing inputs. `
-  }
-  else if (paperMax24.includes(substrate) && Math.min(inchHeight, inchWidth)>23.5) {
-    errorNote = `${substrate} comes in 24" rolls; Your dimension ${Math.min(inputHeight, inputWidth)}${radioChoice} exceeds the printable area (23.5").`
-  }
-  else if (paperMax36.includes(substrate) && Math.min(inchHeight, inchWidth)>35.5) {
-    errorNote = `${substrate} comes in 36" rolls; Your dimension ${Math.min(inputHeight, inputWidth)}${radioChoice} exceeds the printable area (35.5").`
-  }
-  else if(Math.min(inchHeight, inchWidth)>43.5) {
-    errorNote = `${substrate} comes in 44" rolls; Your dimension ${Math.min(inputHeight, inputWidth)}${radioChoice} exceeds the printable area (43.5").`
-  }
-  else if(Math.max(inchHeight, inchWidth)>88) {
-    errorNote = 'Large prints require a custom quotation. Please contact us for details.'
-  }
-  else {errorNote = "no errors";};
-  console.log("errorNote: ", errorNote);
 };
 
-calcBillValues = () => {
+const logInputValues = () => {
+  console.log(`INPUT: ${inputHeight}${radioChoice} by ${inputWidth}${radioChoice} on ${substrate}`)
+};
+
+const displayOutput = () => {
+  checkInput(); // determines error messages
+  estimate = `${inputHeight}x${inputWidth}${radioChoice} on ${substrate}: ${currencyFormat(printCost)}`;
+  if (errorNote === "no errors") {
+    addItem();
+  } else {
+  document.querySelector("#errorNote").innerHTML = `<p style="color:red">${errorNote}</p>`
+  }
+};
+
+//Add items the estimat list
+const addItem = (event) => {
+  const newItem = document.createElement("div");
+  newItem.classList.add("newItem")
+  const newLi = document.createElement("li");
+  newLi.innerText = estimate;
+  const delButton = document.createElement("button") 
+  delButton.setAttribute("title", "Remove this item from the list");
+  delButton.classList.add("delButton")
+  delButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  list.appendChild(newItem);
+  newItem.appendChild(newLi);
+  newItem.appendChild(delButton);
+  document.querySelector("#errorNote").innerHTML = "";
+};
+
+const delItem = (e) => {
+  const clickedItem = e.target;
+  if (clickedItem.classList[0]==="delButton") {
+    const delTarget = clickedItem.parentElement;
+    delTarget.remove();
+  }
+};
+
+const checkInput = () => { // checking if inputs are valid and generate error messages.
+  inchHeight = inputHeight;
+  inchWidth = inputWidth; 
+  if (radioChoice === "mm") {
+    inchHeight = (inputHeight / 25.4).toFixed(2);
+    inchWidth = (inputWidth / 25.4).toFixed(2);
+    //console.log(`Converted inputs:${inchHeight}" x ${inchWidth}"`)
+  }
+  if (!billArea>0 || substrate === "not-valid") {
+    errorNote = `Please enter valid dimensions and select substrate. `
+  }
+  else if (paperMax24.includes(substrate) && Math.min(inchHeight, inchWidth)>23.5) {
+    errorNote = `${substrate} comes in 24" rolls; <br>Your dimension ${Math.min(inputHeight, inputWidth)}${radioChoice} exceeds the printable area (23.5").`
+  }
+  else if (paperMax36.includes(substrate) && Math.min(inchHeight, inchWidth)>35.5) {
+    errorNote = `${substrate} comes in 36" rolls; <br>Your dimension ${Math.min(inputHeight, inputWidth)}${radioChoice} exceeds the printable area (35.5").`
+  }
+  else if(Math.min(inchHeight, inchWidth)>43.5) {
+    errorNote = `${substrate} comes in 44" rolls; <br>Your dimension ${Math.min(inputHeight, inputWidth)}${radioChoice} exceeds the printable area (43.5").`
+  }
+  else if(Math.max(inchHeight, inchWidth)>88) {
+    errorNote = 'Large prints require a custom quotation. <br>Please contact us for details.'
+  }
+  else {errorNote = "no errors";};
+};
+
+const calcBillValues = () => {
   inputArea = inputHeight * inputWidth;
   radioChoice === "mm" ? billArea = Math.ceil(inputArea / 645.16) : billArea = Math.ceil(inputArea);
   if (billArea < 80) {billArea = 80}
   setCPI();
   printCost = billArea * cpi;
-}
+};
 
-logBillValues = () => {
-  console.log(`BILLING: billArea:${billArea}, cpi: ${cpi}, printCost: ${currencyFormat(printCost)}`);
-}
+const logBillValues = () => {
+  console.log(`BILLING: billArea:${billArea}, cpi: ${cpi}, printCost: ${currencyFormat(printCost)}, Total: ${total}`);
+};
 
-runEstimate = () => {
+const runEstimate = () => {
   getInputs();
-  logInputValues();
   calcBillValues();
+  logInputValues();
   logBillValues();
   displayOutput();
   resetFields();
-};  
+};
 
-button.addEventListener("click", runEstimate);
+radios.forEach(radio => radio.addEventListener('change', () => {
+  document.querySelector(`#inputHeight`).placeholder = radio.value;
+  document.querySelector(`#inputWidth`).placeholder = radio.value;
+}));
 
-// TODO: Format inputs in html to accept only valid decimals on inch values, and no decimals on mm values
+estButton.addEventListener("click", runEstimate);
+list.addEventListener("click", delItem);
